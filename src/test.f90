36e837af20ad6391cd520442f3a970f9cd51c30c
Zhouteng Ye <yztchina@zju.edu.cn>
2020-06-04 11:48:54 +0800

    working on interactive view

--- src/test.f90
+++ src/test.f90
@@ -12,251 +12,75 @@ program test
   use ModGlobal
   Implicit None
 
-
   call test_case
 
 end program test
 
 !===============
-! test1: HDF5 parallel input
-! Object:
-!    (1) Determine the i,j,k rank. Especially between n and dims, fortran and python
-!    (2) for a given 2D or 3D shape, able to write correctly
+! test1: Primary test
+!    (1) Read data set correctly
+!    (2) Write data set correctly (see jupyter)
+!    (3) cube advection
+! setup:
+!     20 30 16 grids
+!     u=v=w=1.0
+!     dx=dy=dz=1.0
+!     dx=dy=dz=1.0
+!     dt = 0.2
+!     tend = 2
+!     phi[6:10,6:10,6:10] = 1
+!     
 !===============
 Subroutine test1
-  use ModGlobal
+  Use ModGlobal
+  Use ModTools
+  Use ModVOF
   Implicit None
-  real(SP),allocatable :: p(:,:,:)
   Character(80) :: data_name
-
-  Call MPI_INIT(ierr)
-  call MPI_COMM_RANK(MPI_COMM_WORLD,myid,ierr)
-  call MPI_COMM_SIZE(MPI_COMM_WORLD,nproc,ierr)
-
-  ! ! case 1
-  n = (/4,8,2/)
-  dims = (/2,2/)
-  ! ! case 2
-  ! n = (/4,8,2/)
-  ! dims = (/4,1/)
-  ! ! case 3
-  ! n = (/4,8,1/)
-  ! dims = (/1,1/)
-
-
-  Call InitMPI(n)
-
-
-  Allocate(p(0:nl(1)+1,0:nl(2)+1,0:nl(3)+1))
-
-  p = ToFloat(myid)
-
-
-  ! do i = 1,4
-  !   if (myid .eq. i-1) then
-  !     print *, myid
-  !     print *,p
-  !   end if
-  !   call MPI_Barrier(MPI_COMM_WORLD, ierr)
-  ! end do
-
-
-  Call H5Init(inputfield=.false.)
-  Allocate(h5_output_field(1))
-  h5_output_field%groupname = 'phi'
-  print *, h5_output_field(1)%groupname
-  Call HDF5CreateGroup(h5_output, h5_output_field(1))
-
-  data_name = 'test'
-  Call HDF5WriteData(h5_output_field(1), p, data_name)
-
-  Call MPI_FINALIZE(ierr)
-
-End Subroutine test1
-
-!===============
-! test2: HDF5 parallel input
-! Object:
-!    (1) Read attributes from namelist
-!    (2) Read field from h5file
-!    (3) Assign correct values to each processor
-!===============
-Subroutine test2
-  use ModGlobal
-  Implicit None
-  real(SP),allocatable :: p(:,:,:)
-  Character(80) :: data_name
-
-  Call MPI_INIT(ierr)
-  call MPI_COMM_RANK(MPI_COMM_WORLD,myid,ierr)
-  call MPI_COMM_SIZE(MPI_COMM_WORLD,nproc,ierr)
-
-
-  Call Init_param
-
-
-  if (myid .eq. 0) print *, '======nx, ny, nz======'
-  Call MPI_Barrier(MPI_COMM_WORLD, ierr)
-  print *, myid, n
-  Call MPI_Barrier(MPI_COMM_WORLD, ierr)
-
-  if (myid .eq. 0) print *, '======dx, dy, dz======'
-  Call MPI_Barrier(MPI_COMM_WORLD, ierr)
-  print *, myid, dl
-  Call MPI_Barrier(MPI_COMM_WORLD, ierr)
-
-  if (myid .eq. 0) print *, '======dt======'
-  Call MPI_Barrier(MPI_COMM_WORLD, ierr)
-  print *, myid, dt
-  Call MPI_Barrier(MPI_COMM_WORLD, ierr)
-
-  if (myid .eq. 0) print *, '======px, py======'
-  Call MPI_Barrier(MPI_COMM_WORLD, ierr)
-  print *, myid, dims
-  Call MPI_Barrier(MPI_COMM_WORLD, ierr)
-
-
-  Call InitMPI(n)
-
-  Allocate(p(0:nl(1)+1,0:nl(2)+1,0:nl(3)+1))
-
-  if (myid .eq. 0) print *, '======local computaional grid======'
-  Call MPI_Barrier(MPI_COMM_WORLD, ierr)
-  print *, myid, nl
-  Call MPI_Barrier(MPI_COMM_WORLD, ierr)
-
-  if (myid .eq. 0) print *, '======local array size======'
-  Call MPI_Barrier(MPI_COMM_WORLD, ierr)
-  print *, myid, shape(p)
-  Call MPI_Barrier(MPI_COMM_WORLD, ierr)
-
-  Call H5Init(inputfield=.true.)
-  Call HDF5OpenGroup(h5_input, h5_input_field(1))
-
-  data_name = 'test'
-  Call HDF5ReadData(h5_input_field(1), p, data_name)
-
-  block
-    real(sp) :: out_p(nl(1),nl(2),nl(3))
-    integer :: i
-    out_p(1:nl(1),1:nl(2),1:nl(3)) = p(1:nl(1),1:nl(2),1:nl(3))
-    do i = 1,4
-      if (myid .eq. i-1) then
-        print *, myid
-        print *,out_p
-      end if
-      call MPI_Barrier(MPI_COMM_WORLD, ierr)
-    end do
-  end block
-
-  ! data_name = 'test'
-  ! Call HDF5WriteData(h5_output_phi, p, data_name)
-
-  Call MPI_FINALIZE(ierr)
-
-End Subroutine test2
-
-!===============
-! test3: Initialization for all
-! Object:
-!    (1) Read everything correctly
-!===============
-Subroutine test3
-  use ModGlobal
-  Implicit None
 
   Call Init(inputfield=.true.)
 
+  ! Check if phi is loaded correctly
+   ! print *, phi(6,5,6)
+  if ( myid .eq. 0) then
+    print *, nl
+    print *, dl
+    print *, dt
+    print *, time
+    print *, tend
+  endif
+
+  Call Visual3D(phi)
+
+  ! VOF advection
+  Do While (time < tend)
+    ! call AdvSZ(u, phi, nl, dl, dt, 1)
+    ! call AdvSZ(v, phi, nl, dl, dt, 2)
+    ! call AdvSZ(w, phi, nl, dl, dt, 3)
+    Call APPLIC(Phi, u, v, w, nl, dl, dt)
+    time =  time + dt
+    print *, time
+  End Do
+
   block
-    real(sp) :: out_p(nl(1),nl(2),nl(3))
-    integer :: i
-    ! out_p(1:nl(1),1:nl(2),1:nl(3)) = phi(1:nl(1),1:nl(2),1:nl(3))
-    ! out_p(1:nl(1),1:nl(2),1:nl(3)) = u(1:nl(1),1:nl(2),1:nl(3))
-    out_p(1:nl(1),1:nl(2),1:nl(3)) = v(1:nl(1),1:nl(2),1:nl(3))
-    do i = 1,4
-      if (myid .eq. i-1) then
-        print *, myid
-        print *,out_p
-      end if
-      call MPI_Barrier(MPI_COMM_WORLD, ierr)
-    end do
+    integer :: j
+    Real(sp) :: data_out(nl(1),nl(2),nl(3))
+    data_out(:,:,:) = u(1:nl(1),1:nl(2),1:nl(3))
+  if (myid .eq. 0) then
+      do j = 5, 10
+        ! print *, data_out(:,j,1)
+        print *, u(5:10,j,7)
+      end do
+  end if
   end block
+
+
+  data_name = 'final'
+  Call HDF5WriteData(h5_output_field(1), phi, data_name)
+  Call HDF5WriteData(h5_output_field(2),   u, data_name)
+  Call HDF5WriteData(h5_output_field(3),   v, data_name)
+  Call HDF5WriteData(h5_output_field(4),   w, data_name)
 
   Call MPI_FINALIZE(ierr)
 
-end Subroutine test3
-
-
-!===============
-! test4: boundary conditions
-! Object:
-!     (1) MPI inner boundary
-!     (2) outer boundary
-!===============
-Subroutine test4
-  use ModGlobal
-  Implicit None
-  ! Integer :: nexch(2)
-
- Call Init(inputfield=.true.)
- Call InitBound
-
- ! ! Test Dilichelet 
- ! if all comment, it will use the default Nuemann
- phi_bc%bound_type(:,:) = 1
- phi_bc%bound_value(:,:) = 10
- cx_bc%bound_type(:,:) = 1
- cx_bc%bound_value(:,:) = 10
- cy_bc%bound_type(:,:) = 1
- cy_bc%bound_value(:,:) = 10
- u_bc%bound_type(:,:) = 1
- u_bc%bound_value(:,:) = 10
- v_bc%bound_type(:,:) = 1
- v_bc%bound_value(:,:) = 10
-
- ! ! Do this test first, then apply the SetBCS fubction
- ! nexch = nl(1:2)
- ! call updthalo(nexch,1,u)
- ! call updthalo(nexch,2,u)
-
- ! if(left .eq.MPI_PROC_NULL)  Call u_bc%setBC(1, -1, u)
- ! if(right .eq.MPI_PROC_NULL) Call u_bc%setBC(1, 1, u)
- ! if(front .eq.MPI_PROC_NULL) Call u_bc%setBC(2, -1, u)
- ! if(back .eq.MPI_PROC_NULL)  Call u_bc%setBC(2, 1, u)
- ! Call u_bc%setBC(3, -1, u)
- ! Call u_bc%setBC(3, 1, u)
-
- ! After testing, we should be able to use a simplified Version
- Call u_bc%setBCS(u)
- Call v_bc%setBCS(v)
- Call phi_bc%setBCS(phi)
-
-  block
-    integer :: j,k
-    If (myid .eq. 0) Print *, 'Dir 1: bc_node: ', phi_bc%lohi(1,1), phi_bc%lohi(1,2)
-    If (myid .eq. 0) Print *, 'Dir 2: bc_node: ', phi_bc%lohi(2,1), phi_bc%lohi(2,2)
-    If (myid .eq. 0) Print *, 'Dir 3: bc_node: ', phi_bc%lohi(3,1), phi_bc%lohi(3,2)
-    ! If (myid .eq. 0) Print *, 'Dir 1: bc_node: ', u_bc%lohi(1,1), u_bc%lohi(1,2)
-    ! If (myid .eq. 0) Print *, 'Dir 2: bc_node: ', u_bc%lohi(2,1), u_bc%lohi(2,2)
-    ! If (myid .eq. 0) Print *, 'Dir 3: bc_node: ', u_bc%lohi(3,1), u_bc%lohi(3,2)
-    ! If (myid .eq. 0) Print *, 'Dir 1: bc_node: ', v_bc%lohi(1,1), v_bc%lohi(1,2)
-    ! If (myid .eq. 0) Print *, 'Dir 2: bc_node: ', v_bc%lohi(2,1), v_bc%lohi(2,2)
-    ! If (myid .eq. 0) Print *, 'Dir 3: bc_node: ', v_bc%lohi(3,1), v_bc%lohi(3,2)
-    do k = 1,4
-      if (myid .eq. k-1) then
-        print *, myid
-        do j = 0, nl(2)+1
-          ! print *, phi(:,j,2)
-          ! print *, phi(:,j,0)
-          print *, phi(:,j,nl(3))
-          ! print *, phi(:,j,0) - phi(:,j,1)
-          ! print *, phi(:,j,nl(3)+1) - phi(:,j,nl(3))
-        end do
-      end if
-      call MPI_Barrier(MPI_COMM_WORLD, ierr)
-    end do
-  end block
-
-  Call MPI_FINALIZE(ierr)
-
-end Subroutine test4
+end Subroutine test1
