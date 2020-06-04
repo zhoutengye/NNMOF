1841d32b721dee9179698fe6102a7756c995c07a
Zhouteng Ye <yztchina@zju.edu.cn>
2020-06-04 23:30:34 +0800

    simplified Advection subroutine

--- src/vof_adv.f90
+++ src/vof_adv.f90
@@ -3,7 +3,7 @@
 !=======================================================
 SUBROUTINE AdvSZ(us, c, nl, dl, dt, dir)
   Use ModGlobal, only : sp
-  Use ModGlobal, only : updthalo
+  Use ModGlobal, only : setBCS, phi_bc
   Implicit None
   Integer :: dir
   Real(sp) :: dt
@@ -11,167 +11,110 @@ SUBROUTINE AdvSZ(us, c, nl, dl, dt, dir)
   Real(sp),Intent(In)    :: dl(3)
   Real(sp),Intent(In)    :: us(0:nl(1)+1,0:nl(2)+1,0:nl(3)+1)
   Real(sp),Intent(InOut) :: c(0:nl(1)+1,0:nl(2)+1,0:nl(3)+1)
-  Real(sp) :: mx,my,mz,mm1,mm2
-  Integer  :: i,j,k,invx,invy,invz
+  Real(sp) :: mm1,mm2
+  Integer  :: i, j, k, ii, jj, kk, ic1, ic2, ic3
   Real(sp) :: a1,a2,alpha
   Real(sp),dimension(0:nl(1)+1,0:nl(2)+1,0:nl(3)+1) :: vof1,vof2,vof3
-  Real(sp) :: norm(3)
+  Real(sp) :: norm(3), abs_norm(3)
   Real(sp) :: c_block(3,3,3)
   Real(sp) :: FloodAPPLIC_Backward, FloodAPPLIC_Forward
   Integer :: nexch(2)
   nexch = nl(1:2)
 
-  vof1 = 0.0d0
-  vof2 = 0.0d0
-  vof3 = 0.0d0
+  vof1 = 0.0_sp
+  vof2 = 0.0_sp
+  vof3 = 0.0_sp
+
+  if (dir.eq.1)  then
+    ii  = 1
+    ic1 = 1
+    ic2 = 2
+    ic3 = 3
+  else
+    ii = 0
+  end if
+  if (dir.eq.2)  then
+    jj  = 1
+    ic1 = 2
+    ic2 = 3
+    ic3 = 1
+  else
+    jj = 0
+  end if
+  if (dir.eq.3)  then
+    kk  = 1
+    ic1 = 3
+    ic2 = 1
+    ic3 = 2
+  else
+    kk = 0
+  end if
+
   !***
   do k=1,nl(3)
     do j=1,nl(2)
       do i=1,nl(1)
         a1 = us(i,j,k) *dt/dl(1)
-        if (dir.eq.1) then
-          a2 = us(i+1,j,k) *dt/dl(1)
-        elseif (dir.eq.2) then
-          a2 = us(i,j+1,k) *dt/dl(2)
-        elseif (dir.eq.3) then
-          a2 = us(i,j,k+1) *dt/dl(3)
-        endif
+        a2 = us(i+ii,j+jj,k+kk) *dt/dl(dir)
 
         !***
         !     3 cases: 1: DEFAULT (c=0. and fluxes=0.); 2: c=1.; 3:c>0.
         !***
 
-        if (c(i,j,k) .EQ. 1.0d0) then
-          vof1(i,j,k) = DMAX1(-a1,0.d0)
-          vof2(i,j,k) = 1.d0 - DMAX1(a1,0.d0) + DMIN1(a2,0.d0)
-          vof3(i,j,k) = DMAX1(a2,0.d0)
+        if (c(i,j,k) .EQ. 1.0_sp) then
+          vof1(i,j,k) = DMAX1(-a1,0.0_sp)
+          vof2(i,j,k) = 1.0_sp - DMAX1(a1,0.0_sp) + DMIN1(a2,0.0_sp)
+          vof3(i,j,k) = DMAX1(a2,0.0_sp)
 
-        else if (c(i,j,k) .GT. 0.d0) then
+        else if (c(i,j,k) .GT. 0.0_sp) then
           !***
-          !     (1) normal vector: mx,my,mz; (2) mx,my,mz>0. and mx+my+mz = 1.;
-          !     (3) get alpha;               (4) back to original plane;
-          !     (5) lagrangian advection;    (6) get fluxes
+          !     ;    (6) get fluxes
 
-          !*(1)*
+          !*(1)* normal vector
           c_block = c(i-1:i+1,j-1:j+1,k-1:k+1)
-          ! Call NormParkerYoungs(c, norm)
-          Call NormAPPLIC(c, norm)
-                  ! mm1 = c(i-1,j-1,k-1)+c(i-1,j-1,k+1)+c(i-1,j+1,k-1) & 
-                  !     +c(i-1,j+1,k+1)+2.0d0*(c(i-1,j-1,k)+c(i-1,j+1,k)&
-                  !     +c(i-1,j,k-1)+c(i-1,j,k+1))+4.0d0*c(i-1,j,k)
-                  ! mm2 = c(i+1,j-1,k-1)+c(i+1,j-1,k+1)+c(i+1,j+1,k-1) &
-                  !     +c(i+1,j+1,k+1)+2.0d0*(c(i+1,j-1,k)+c(i+1,j+1,k) &
-                  !     +c(i+1,j,k-1)+c(i+1,j,k+1))+4.0d0*c(i+1,j,k)
-                  ! mx = mm1 - mm2
-                  
-                  ! mm1 = c(i-1,j-1,k-1)+c(i-1,j-1,k+1)+c(i+1,j-1,k-1) &
-                  !     +c(i+1,j-1,k+1)+2.0d0*(c(i-1,j-1,k)+c(i+1,j-1,k) &
-                  !     +c(i,j-1,k-1)+c(i,j-1,k+1))+4.0d0*c(i,j-1,k)
-                  ! mm2 = c(i-1,j+1,k-1)+c(i-1,j+1,k+1)+c(i+1,j+1,k-1) &
-                  !     +c(i+1,j+1,k+1)+2.0d0*(c(i-1,j+1,k)+c(i+1,j+1,k) &
-                  !     +c(i,j+1,k-1)+c(i,j+1,k+1))+4.0d0*c(i,j+1,k)
-                  ! my = mm1 - mm2
-                  
-                  ! mm1 = c(i-1,j-1,k-1)+c(i-1,j+1,k-1)+c(i+1,j-1,k-1) &
-                  !     +c(i+1,j+1,k-1)+2.0d0*(c(i-1,j,k-1)+c(i+1,j,k-1) &
-                  !     +c(i,j-1,k-1)+c(i,j+1,k-1))+4.0d0*c(i,j,k-1)
-                  ! mm2 = c(i-1,j-1,k+1)+c(i-1,j+1,k+1)+c(i+1,j-1,k+1) &
-                  !     +c(i+1,j+1,k+1)+2.0d0*(c(i-1,j,k+1)+c(i+1,j,k+1) &
-                  !     +c(i,j-1,k+1)+c(i,j+1,k+1))+4.0d0*c(i,j,k+1)
-                  ! mz = mm1 - mm2
-          ! mx = norm(1)
-          ! my = norm(2)
-          ! mz = norm(3)
+          Call NormParkerYoungs(c_block, norm)
+          ! Call NormAPPLIC(c_block, norm)
 
-          !*(2)*
-          invx = 1
-          invy = 1
-          invz = 1
-          if (mx .LT. 0.0d0) then
-            mx = -mx
-            invx = -1
-          endif
-          if (my .LT. 0.0d0) then
-            my = -my
-            invy = -1
-          endif
-          if (mz .LT. 0.0d0) then
-            mz = -mz
-            invz = -1
-          endif
-          mm2 = mx+my+mz
-          mx = mx/mm2
-          my = my/mm2
-          mz = mz/mm2
+          !*(2) mx,my,mz>0. and mx+my+mz = 1.;
+          Call Normalization1(norm, abs_norm)
 
-          !*(3)*  
-          alpha = FloodAPPLIC_Backward(mx,my,mz,c(i,j,k))
+          !*(3) get alpha;
+          alpha = FloodAPPLIC_Backward(abs_norm(1),abs_norm(2),abs_norm(3),c(i,j,k))
 
-          !*(4)*  
-          mx = invx*mx
-          my = invy*my
-          mz = invz*mz
-          alpha = alpha + DMIN1(0.d0,mx) + DMIN1(0.d0,my) + &
-          &                 DMIN1(0.d0,mz)
+          !*(4) back to original plane;
+          alpha = alpha + DMIN1(0.0_sp,norm(1)) + DMIN1(0.0_sp,norm(2)) + &
+          &                 DMIN1(0.0_sp,norm(3))
 
-          !*(5)*
-          mm1 = DMAX1(a1,0.0d0)
-          mm2 = 1.d0 - mm1 + DMIN1(0.d0,a2)
+          !*(5) semi-lagrangian advection
+          mm1 = DMAX1(a1,0.0_sp)
+          mm2 = 1.0_sp - mm1 + DMIN1(0.0_sp,a2)
 
-          if (dir.eq.1) then
-            mx = mx/(1.0d0 - a1 + a2)
-            alpha = alpha + mx*a1
-            if (a1 .LT. 0.d0) vof1(i,j,k) = FloodAPPLIC_Forward(mx,my,mz,alpha,a1  ,-a1)
-            if (a2 .GT. 0.d0) vof3(i,j,k) = FloodAPPLIC_Forward(mx,my,mz,alpha,1.d0,a2)
-            vof2(i,j,k) = FloodAPPLIC_Forward(mx,my,mz,alpha,mm1,mm2)
-          elseif (dir.eq.2) then
-            my = my/(1.0d0 - a1 + a2)
-            alpha = alpha + my*a1
-            if (a1 .LT. 0.d0) vof1(i,j,k) = FloodAPPLIC_Forward(my,mz,mx,alpha,a1  ,-a1)
-            if (a2 .GT. 0.d0) vof3(i,j,k) = FloodAPPLIC_Forward(my,mz,mx,alpha,1.d0,a2)
-            vof2(i,j,k) = FloodAPPLIC_Forward(my,mz,mx,alpha,mm1,mm2)
-          elseif (dir.eq.3) then
-            mz = mz/(1.0d0 - a1 + a2)
-            alpha = alpha + mz*a1
-            if (a1 .LT. 0.d0) vof1(i,j,k) = FloodAPPLIC_Forward(mz,mx,my,alpha,a1  ,-a1)
-            if (a2 .GT. 0.d0) vof3(i,j,k) = FloodAPPLIC_Forward(mz,mx,my,alpha,1.d0,a2)
-            vof2(i,j,k) = FloodAPPLIC_Forward(mz,mx,my,alpha,mm1,mm2)
-          endif
+          !*(6) get fluxes
+          norm(dir) = norm(dir)/(1.0_sp - a1 + a2)
+          alpha = alpha + norm(dir)*a1
+          if (a1 .LT. 0.0_sp) vof1(i,j,k) = FloodAPPLIC_Forward(norm(ic1),norm(ic2),norm(ic3),alpha,a1  ,-a1)
+          if (a2 .GT. 0.0_sp) vof3(i,j,k) = FloodAPPLIC_Forward(norm(ic1),norm(ic2),norm(ic3),alpha,1.0_sp,a2)
+          vof2(i,j,k) = FloodAPPLIC_Forward(norm(ic1),norm(ic2),norm(ic3),alpha,mm1,mm2)
         endif
       enddo
     enddo
   enddo
-  call updthalo(nexch,1,vof1)
-  call updthalo(nexch,2,vof1)
-  call updthalo(nexch,1,vof2)
-  call updthalo(nexch,2,vof2)
-  call updthalo(nexch,1,vof3)
-  call updthalo(nexch,2,vof3)
-  !***
-  !     (1) apply proper boundary conditions to fluxes
-  !     (2) new values of c and  clip it: 0. <= c <= 1.
-  !     (3) apply proper boundary conditions to c
-  !*(1)*
-  ! call bc_flux(vof1,vof3,us,nx,ny,nz,d)
- !*(2)*
+
+  ! apply proper boundary conditions to vof1, vof2, vof3
+  call phi_bc%setBCS(vof1)
+  call phi_bc%setBCS(vof2)
+  call phi_bc%setBCS(vof3)
+  !new values of c and  clip it: 0. <= c <= 1.
   do k=1,nl(3)
     do j=1,nl(2)
       do i=1,nl(1)
-        if (dir.eq.1) then
-          c(i,j,k) = vof3(i-1,j,k) + vof2(i,j,k) + vof1(i+1,j,k)
-        elseif (dir.eq.2) then
-          c(i,j,k) = vof3(i,j-1,k) + vof2(i,j,k) + vof1(i,j+1,k)
-        elseif (dir.eq.3) then
-          c(i,j,k) = vof3(i,j,k-1) + vof2(i,j,k) + vof1(i,j,k+1)
-        endif
-        c(i,j,k) = DMAX1(0.0d0,DMIN1(1.0d0,c(i,j,k)))
+        c(i,j,k) = vof3(i-ii,j-jj,k-kk) + vof2(i,j,k) + vof1(i+ii,j+jj,k+kk)
+        c(i,j,k) = DMAX1(0.0_sp,DMIN1(1.0_sp,c(i,j,k)))
       enddo
     enddo
   enddo
-  !*(3)*
-  ! call bc_c(c,nx,ny,nz)
-  call updthalo(nexch,1,c)
-  call updthalo(nexch,2,c)
+  ! apply proper boundary conditions to c
+  call phi_bc%setBCS(c)
   !***
   return
 End Subroutine AdvSZ
--- src/vof_norm.f90
+++ src/vof_norm.f90
@@ -3,18 +3,21 @@
   !    sigma norm(i) = 1
   ! and
   !    all norm(i) > 0
-  Subroutine Normalization1(norm)
+  Subroutine Normalization1(norm, abs_norm)
     Use ModGlobal, only : sp
     Implicit None
     Real(sp), Intent(Inout) :: norm(3)
+    Real(sp), Intent(Out) :: abs_norm(3)
     Real(sp) :: aa
-    norm = abs(norm)
-    aa = norm(1) + norm(2) + norm(3)
+
+    abs_norm = abs(norm)
+    aa = abs_norm(1) + abs_norm(2) + abs_norm(3)
     If( aa .gt. 1.d-10) Then
-       norm(1) = norm(1) / aa
-       norm(2) = norm(2) / aa
-       norm(3) = norm(3) / aa
+      norm = norm / aa
+      abs_norm = abs_norm / aa
     End If
+
+
   End Subroutine Normalization1
 
   !=======================================================
@@ -62,7 +65,7 @@
         +f(3,3,3)+2.0_sp*(f(1,3,2)+f(3,3,2) &
         +f(2,3,1)+f(2,3,3))+4.0_sp*f(2,3,2)
     norm(2) = mm1 - mm2
-                  
+
     mm1 = f(1,1,1)+f(1,3,1)+f(3,1,1) &
         +f(3,3,1)+2.0_sp*(f(1,2,1)+f(3,2,1) &
         +f(2,1,1)+f(2,3,1))+4.0_sp*f(2,2,1)
