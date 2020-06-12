c7ac64a3ae92519f066239a99df66e957b7a36a6
zhoutengye <yztchina@zju.edu.cn>
2020-06-12 22:12:01 +0800

    start first case

---
+++ cases/gen_data/gen_data.py
@@ -0,0 +1,12 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import random
+import mof
+
+flood_sz = floo
+
+class mof_data:
+    def __init__(self, len_dataset):
+        self.n_data = len_dataset
+
+    def linear(self):
