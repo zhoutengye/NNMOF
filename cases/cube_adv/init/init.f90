e1061373b8d6074a993bef90ed1f1562c5698205
Zhouteng Ye <yztchina@zju.edu.cn>
2020-06-14 02:36:52 +0800

    Tedious case set up

--- cases/cube_adv/init/init.f90
+++ cases/cube_adv/init/init.f90
@@ -7,11 +7,47 @@ Contains
     ShapeTest = 1.0_sp-x-y-z
   End Function ShapeTest
 
+  Real(8) Function ShapeCube1(x,y,z)
+    Implicit None
+    Real(8) :: x, y, z
+    ShapeCube1 = min(x, min(y, z))
+  End Function ShapeCube1
+
+  Real(8) Function ShapeCube2(x,y,z)
+    Implicit None
+    Real(8) :: x, y, z
+    ShapeCube2 = min(0.5d0-x, min(0.5d0-y, 0.5d0-z))
+  End Function ShapeCube2
+
   Real(8) Function ShapeSphere(x,y,z)
     Implicit None
     Real(8) :: x, y, z
-    ShapeSphere = 1.0_sp - sqrt(x**2+y**2+z**2)
+    Real(8) :: sph1, sph2
+    sph1 = 1.0_sp - sqrt(x**2+y**2+z**2)
+    sph2 = sqrt(x**2+y**2+z**2) - 0.5_sp
+    ShapeSphere = min(sph1, sph2)
   End Function ShapeSphere
+
+  Real(8) Function ShapeTCube1(x,y,z)
+    Implicit None
+    Real(8) :: x, y, z
+    ShapeTCube1 = 1.0_sp - (x+y+z)
+  End Function ShapeTCube1
+
+  Real(8) Function ShapeTCube2(x,y,z)
+    Implicit None
+    Real(8) :: x, y, z
+    ShapeTCube2 = 0.5_sp - (x+y+z)
+  End Function ShapeTCube2
+
+  Real(8) Function ShapeLA1(x,y,z)
+    Implicit None
+    Real(8) :: x, y, z
+    Real(8) :: la1, la2
+    la1 = 1.0 - sqrt(y**2+z**2) - x / 2.0_sp
+    la2 =  sqrt(y**2+z**2) + x / 2.0_sp - 0.5_sp
+    ShapeLA1 = min(la1,la2)
+  End Function ShapeLA1
 
 End Module InitSH
 
--- cases/cube_adv/init/main.f90
+++ cases/cube_adv/init/main.f90
@@ -43,7 +43,7 @@ Subroutine test1
   gr%dx(3) = 1.0_sp
   ShapeLevelSet => ShapeTest
 
-  Call VolumeCentroidQuadTree(gr)
+  Call VolumeCentroidOctree(gr)
 
   print *, gr%vof
   print *, gr%centroid
@@ -58,7 +58,7 @@ Subroutine test1
   gr%dx(2) = 1.0_sp
   gr%dx(3) = 1.0_sp
   ShapeLevelSet => ShapeTest
-  Call VolumeQuadTree(gr)
+  Call VolumeOctree(gr)
   print *, gr%vof
 
   ! Call MPI_FINALIZE(ierr)
@@ -70,49 +70,294 @@ Subroutine test2
   Use InitSH
   Implicit None
   Real(sp) :: sph(20,20,20)
-  Real(sp) :: xc(20)
+  Real(sp) :: cube1(20,20,20)
+  Real(sp) :: cube2(20,20,20)
+  Real(sp) :: tcube1(20,20,20)
+  Real(sp) :: tcube2(20,20,20)
+  Real(sp) :: LA1(40,20,20)
+  Real(sp), Allocatable :: xc(:)
   Type(Octree) :: gr
   Integer :: i,j,k
+  Integer :: icc,jcc,kcc
   Integer :: flag
   Real(sp) :: dx
 
   Call Init(inputfield=.false.)
 
+
+  ! ---------------------------------
+  ! Hollow cube
+  Allocate(xc(20))
   Do k = 1, 20
     xc(k) = dble(k) / dble(20) - 0.025_sp
   End Do
-
   dx = 0.05_sp
-
-  ShapeLevelSet => ShapeSphere
-
+  ShapeLevelSet => ShapeCube1
   Do k = 1, 20
     Do j = 1, 20
       Do i = 1, 20
-        flag = Inout(xc(i), xc(j), xc(k), dx, dx, dx)
+        flag = Inout(xc(i), xc(j), xc(k), dx/2.0_sp, dx/2.0_sp, dx/2.0_sp)
         If (flag .eq. 1) Then
-          sph(i,j,k) = 1.0
+          cube1(i,j,k) = 1.0
         Elseif (flag .eq. 3) Then
-          sph(i,j,k) = 0.0
+          cube1(i,j,k) = 0.0
         Elseif (flag .eq. 2) Then
           gr%level = 1
-          gr%maxlevel = 10
+          gr%maxlevel = 5
           gr%xc(1) = xc(i)
           gr%xc(2) = xc(j)
           gr%xc(3) = xc(k)
           gr%dx(1) = dx
           gr%dx(2) = dx
           gr%dx(3) = dx
-          Call VolumeQuadTree(gr)
-          sph(i,j,k) = gr%vof
+          Call VolumeOctree(gr)
+          cube1(i,j,k) = gr%vof
         EndIf
-        phi(i,j,k) = sph(i,j,k)
+      End Do
+    End Do
+  End Do
+  ShapeLevelSet => ShapeCube2
+  Do k = 1, 20
+    Do j = 1, 20
+      Do i = 1, 20
+        flag = Inout(xc(i), xc(j), xc(k), dx/2.0_sp, dx/2.0_sp, dx/2.0_sp)
+        If (flag .eq. 1) Then
+          cube2(i,j,k) = 1.0
+        Elseif (flag .eq. 3) Then
+          cube2(i,j,k) = 0.0
+        Elseif (flag .eq. 2) Then
+          gr%level = 1
+          gr%maxlevel = 5
+          gr%xc(1) = xc(i)
+          gr%xc(2) = xc(j)
+          gr%xc(3) = xc(k)
+          gr%dx(1) = dx
+          gr%dx(2) = dx
+          gr%dx(3) = dx
+          Call VolumeOctree(gr)
+          cube2(i,j,k) = gr%vof
+        EndIf
       End Do
     End Do
   End Do
 
+  icc = 25
+  jcc = 25
+  kcc = 25
+  Do k = 1, 20
+    Do j = 1, 20
+      Do i = 1, 20
+        Phi(icc+i  , jcc+j  , kcc+k  ) = cube1(i,j,k) - cube2(i,j,k)
+        Phi(icc+1-i, jcc+j  , kcc+k  ) = cube1(i,j,k) - cube2(i,j,k)
+        Phi(icc+i  , jcc+1-j, kcc+k  ) = cube1(i,j,k) - cube2(i,j,k)
+        Phi(icc+1-i, jcc+1-j, kcc+k  ) = cube1(i,j,k) - cube2(i,j,k)
+        Phi(icc+i  , jcc+j  , kcc+1-k) = cube1(i,j,k) - cube2(i,j,k)
+        Phi(icc+1-i, jcc+j  , kcc+1-k) = cube1(i,j,k) - cube2(i,j,k)
+        Phi(icc+i  , jcc+1-j, kcc+1-k) = cube1(i,j,k) - cube2(i,j,k)
+        Phi(icc+1-i, jcc+1-j, kcc+1-k) = cube1(i,j,k) - cube2(i,j,k)
+      End Do
+    End Do
+  End Do
+  ! End Hollow cube
+  ! ---------------------------------
+  
+  !----------------------------------
+  ! Hollow Sphere
+  Do k = 1, 20
+    xc(k) = dble(k) / dble(20) - 0.025_sp
+  End Do
+  dx = 0.05_sp
+  ShapeLevelSet => Shapesphere
+  icc = 25
+  jcc = 75
+  kcc = 25
+  Do k = 1, 20
+    Do j = 1, 20
+      Do i = 1, 20
+        flag = Inout(xc(i), xc(j), xc(k), dx/2.0_sp, dx/2.0_sp, dx/2.0_sp)
+        If (flag .eq. 1) Then
+          sph(i,j,k) = 1.0
+        Elseif (flag .eq. 3) Then
+          sph(i,j,k) = 0.0
+        Elseif (flag .eq. 2) Then
+          gr%level = 1
+          gr%maxlevel = 5
+          gr%xc(1) = xc(i)
+          gr%xc(2) = xc(j)
+          gr%xc(3) = xc(k)
+          gr%dx(1) = dx
+          gr%dx(2) = dx
+          gr%dx(3) = dx
+          Call VolumeOctree(gr)
+          sph(i,j,k) = gr%vof
+        EndIf
+        Phi(icc+i  , jcc+j  , kcc+k  ) = sph(i,j,k)
+        Phi(icc+1-i, jcc+j  , kcc+k  ) = sph(i,j,k)
+        Phi(icc+i  , jcc+1-j, kcc+k  ) = sph(i,j,k)
+        Phi(icc+1-i, jcc+1-j, kcc+k  ) = sph(i,j,k)
+        Phi(icc+i  , jcc+j  , kcc+1-k) = sph(i,j,k)
+        Phi(icc+1-i, jcc+j  , kcc+1-k) = sph(i,j,k)
+        Phi(icc+i  , jcc+1-j, kcc+1-k) = sph(i,j,k)
+        Phi(icc+1-i, jcc+1-j, kcc+1-k) = sph(i,j,k)
+      End Do
+    End Do
+  End Do
+  ! End hollow Sphere
+  !----------------------------------
+
+
+  !----------------------------------
+  ! Tilt hollow cube
+  Do k = 1, 20
+    xc(k) = dble(k) / dble(20) - 0.025_sp
+  End Do
+  dx = 0.05_sp
+  ShapeLevelSet => ShapeTCube1
+  Do k = 1, 20
+    Do j = 1, 20
+      Do i = 1, 20
+        flag = Inout(xc(i), xc(j), xc(k), dx/2.0_sp, dx/2.0_sp, dx/2.0_sp)
+        If (flag .eq. 1) Then
+          tcube1(i,j,k) = 1.0
+        Elseif (flag .eq. 3) Then
+          tcube1(i,j,k) = 0.0
+        Elseif (flag .eq. 2) Then
+          gr%level = 1
+          gr%maxlevel = 5
+          gr%xc(1) = xc(i)
+          gr%xc(2) = xc(j)
+          gr%xc(3) = xc(k)
+          gr%dx(1) = dx
+          gr%dx(2) = dx
+          gr%dx(3) = dx
+          Call VolumeOctree(gr)
+          tcube1(i,j,k) = gr%vof
+        EndIf
+      End Do
+    End Do
+  End Do
+  ShapeLevelSet => ShapeTCube2
+  Do k = 1, 20
+    Do j = 1, 20
+      Do i = 1, 20
+        flag = Inout(xc(i), xc(j), xc(k), dx/2.0_sp, dx/2.0_sp, dx/2.0_sp)
+        If (flag .eq. 1) Then
+          tcube2(i,j,k) = 1.0
+        Elseif (flag .eq. 3) Then
+          tcube2(i,j,k) = 0.0
+        Elseif (flag .eq. 2) Then
+          gr%level = 1
+          gr%maxlevel = 5
+          gr%xc(1) = xc(i)
+          gr%xc(2) = xc(j)
+          gr%xc(3) = xc(k)
+          gr%dx(1) = dx
+          gr%dx(2) = dx
+          gr%dx(3) = dx
+          Call VolumeOctree(gr)
+          tcube2(i,j,k) = gr%vof
+        EndIf
+      End Do
+    End Do
+  End Do
+
+  icc = 75
+  jcc = 25
+  kcc = 25
+  Do k = 1, 20
+    Do j = 1, 20
+      Do i = 1, 20
+        Phi(icc+i  , jcc+j  , kcc+k  ) = tcube1(i,j,k) - tcube2(i,j,k)
+        Phi(icc+1-i, jcc+j  , kcc+k  ) = tcube1(i,j,k) - tcube2(i,j,k)
+        Phi(icc+i  , jcc+1-j, kcc+k  ) = tcube1(i,j,k) - tcube2(i,j,k)
+        Phi(icc+1-i, jcc+1-j, kcc+k  ) = tcube1(i,j,k) - tcube2(i,j,k)
+        Phi(icc+i  , jcc+j  , kcc+1-k) = tcube1(i,j,k) - tcube2(i,j,k)
+        Phi(icc+1-i, jcc+j  , kcc+1-k) = tcube1(i,j,k) - tcube2(i,j,k)
+        Phi(icc+i  , jcc+1-j, kcc+1-k) = tcube1(i,j,k) - tcube2(i,j,k)
+        Phi(icc+1-i, jcc+1-j, kcc+1-k) = tcube1(i,j,k) - tcube2(i,j,k)
+      End Do
+    End Do
+  End Do
+  ! End tilt hollow cube
+  !----------------------------------
+
+
+  !----------------------------------
+  ! Tilt hollow cube
+  Deallocate(xc)
+  Allocate(xc(40))
+  Do k = 1, 40
+    xc(k) = dble(k) / dble(20) - 0.025_sp
+  End Do
+  dx = 0.05_sp
+  ShapeLevelSet => ShapeLA1
+  Do k = 1, 20
+    Do j = 1, 20
+      Do i = 1, 40
+        flag = Inout(xc(i), xc(j), xc(k), dx/2.0_sp, dx/2.0_sp, dx/2.0_sp)
+        If (flag .eq. 1) Then
+          LA1(i,j,k) = 1.0
+        Elseif (flag .eq. 3) Then
+          LA1(i,j,k) = 0.0
+        Elseif (flag .eq. 2) Then
+          gr%level = 1
+          gr%maxlevel = 5
+          gr%xc(1) = xc(i)
+          gr%xc(2) = xc(j)
+          gr%xc(3) = xc(k)
+          gr%dx(1) = dx
+          gr%dx(2) = dx
+          gr%dx(3) = dx
+          Call VolumeOctree(gr)
+          LA1(i,j,k) = gr%vof
+        EndIf
+      End Do
+    End Do
+  End Do
+
+  icc = 55
+  jcc = 75
+  kcc = 25
+  Do k = 1, 20
+    Do j = 1, 20
+      Do i = 1, 40
+        Phi(icc+i, jcc+j  , kcc+k  ) = LA1(i,j,k)
+        Phi(icc+i, jcc+1-j, kcc+k  ) = LA1(i,j,k)
+        Phi(icc+i, jcc+j  , kcc+1-k) = LA1(i,j,k)
+        Phi(icc+i, jcc+1-j, kcc+1-k) = LA1(i,j,k)
+      End Do
+    End Do
+  End Do
+  ! End Lettter A
+  !----------------------------------
+
+  Block 
+    Integer :: nn
+    Character(80) :: data_name
+    data_name = 'test'
+    print *, n_vars
+    do nn = 1, n_vars
+      Select Case(Trim(h5_output_field(nn)%groupname))
+      Case('phi')
+        Call HDF5WriteData(h5_output_field(nn), phi,data_name)
+      Case('u')
+        Call HDF5WriteData(h5_output_field(nn), u,data_name)
+      Case('v')
+        Call HDF5WriteData(h5_output_field(nn), v,data_name)
+      Case('w')
+        Call HDF5WriteData(h5_output_field(nn), w,data_name)
+      Case('cx')
+        Call HDF5WriteData(h5_output_field(nn), cx,data_name)
+      Case('cy')
+        Call HDF5WriteData(h5_output_field(nn), cy,data_name)
+      Case('cz')
+        Call HDF5WriteData(h5_output_field(nn), cz,data_name)
+      End Select
+    end do
+  End Block
+
   Call Visual3DContour(f1=phi)
 
-End Subroutine test2
 
+  End Subroutine test2
 
