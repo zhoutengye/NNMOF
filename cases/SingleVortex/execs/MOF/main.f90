1665480c1347c4c7a3a9403b29d0fa035cf5f8f8
zhoutengye <yztchina@zju.edu.cn>
2020-06-17 22:13:45 +0800

    working on single vortex

---
+++ cases/SingleVortex/execs/MOF/main.f90
@@ -0,0 +1,91 @@
+# include "param.h"
+program main
+  use ModGlobal
+  Implicit None
+
+  call zalesak
+
+end program main
+
+Subroutine zalesak
+  Use ModGlobal
+  Use ModTools
+  Use ModVOF
+  Implicit None
+  Real(sp), allocatable, Dimension(:,:,:) :: f_beg
+  Real(sp), allocatable, Dimension(:,:,:) :: f_end
+  Real(sp), allocatable, Dimension(:,:,:) :: f_exact
+  Real(sp) :: v1, v2
+  Real(sp) :: v11, v12
+  Integer  :: nn = 0
+  Character(80) :: data_name
+  Integer :: i, j, k
+  Real(sp) :: err
+  Real(sp) :: tt1, tt2
+  Integer :: rank
+
+  Call Init(inputfield=.true.)
+
+  Call u_bc%SetBCS(u)
+  Call v_bc%SetBCS(v)
+  Call w_bc%SetBCS(w)
+  Call phi_bc%SetBCS(phi)
+  Call phi_bc%SetBCS(cx)
+  Call phi_bc%SetBCS(cy)
+  Call phi_bc%SetBCS(cz)
+  Allocate(f_beg(0:nl(1)+1,0:nl(2)+1,0:nl(3)+1))
+  Allocate(f_end(0:nl(1)+1,0:nl(2)+1,0:nl(3)+1))
+  Allocate(f_exact(0:nl(1)+1,0:nl(2)+1,0:nl(3)+1))
+
+  u = -u
+  v = -v
+  w = -w
+
+  f_beg = phi
+  f_exact = f_beg
+
+  ! VOF advection
+  Call CPU_Time(tt1)
+  Do While (time < tend)
+    if (myid .eq. 0) print *, 'step =', nn
+    rank = mod(nn+1,3)
+    Call MOFWY2(Phi, cx, cy, cz, u, v, w, nl, dl, dt,rank)
+    nn = nn + 1
+    time =  time + dt
+  End Do
+  Call CPU_Time(tt2)
+
+  data_name = 'final'
+  Call HDF5WriteFrame(data_name)
+
+  f_end = phi
+  err = 0.0_sp
+  Do k = 1, nl(3)
+    Do j = 1, nl(2)
+      Do i = 1, nl(1)
+        err = err + abs(f_end(i,j,k)-f_exact(i,j,k))
+      End Do
+    End Do
+  End Do
+
+  v1 = sum(f_beg(1:nl(1),1:nl(2),1:nl(3)))
+  v2 = sum(f_end(1:nl(1),1:nl(2),1:nl(3)))
+
+  Call MPI_Reduce(v1, v11, 1, MPI_REAL_SP, MPI_SUM, MPI_COMM_WORLD, 0, ierr)
+  Call MPI_Reduce(v1, v12, 1, MPI_REAL_SP, MPI_SUM, MPI_COMM_WORLD, 0, ierr)
+  if (myid .eq.0) then
+    print *, 'cpu_time =', tt2-tt1
+    print *, 'Initial volume:', v11
+    print *, 'Initial volume:', v12
+    print *, err
+  endif
+
+  Call Visual3DContour(f1=f_end)
+  Call Visual2DContour(f1=f_end, slice_dir=3, slice_coord=nl(3)/2)
+
+
+  Call MPI_FINALIZE(ierr)
+
+
+end Subroutine zalesak
+
---
+++ cases/SingleVortex/execs/init/main.f90
@@ -0,0 +1,134 @@
+# define epsc 1.0e-12
+# define Pi 3.1415926535897932d0
+program RiderKotheInit
+  use ModGlobal
+  Implicit None
+
+  ! numer of grids
+  call InitVolume
+
+end program RiderKotheInit
+
+
+Subroutine InitVolume
+  Use ModTools
+  Use ModVOFFunc
+  Use InitSH
+  Implicit None
+  Real(sp), Allocatable :: xc(:)
+  Real(sp), Allocatable :: xl(:)
+  Real(sp), Allocatable :: ls(:,:,:)
+  Type(Octree) :: gr
+  Integer :: i,j,k
+  Integer :: flag
+  Real(sp) :: dx
+  Integer :: Octreelevel = 3
+  Real(sp) :: xx, yy, zz
+  Real(sp) :: n3(3), c3(3)
+  Integer :: ng
+
+  Call Init(inputfield=.false.)
+
+  ng = nl(1)
+  Allocate(ls(0 :ng+1,0:ng+1,0:ng+1))
+
+  dx = 1.0_sp / dble(ng)
+  Allocate(xc(ng))
+  Allocate(xl(ng))
+  Do k = 1, ng
+    xc(k) = dble(k)/ dble(ng) - 0.5_sp * dx
+    xl(k) = dble(k)/ dble(ng) - 1.0_sp * dx
+  End Do
+  dx = 1.0_sp / dble(ng)
+  ShapeLevelSet => ShapeRiderKothe
+
+  Do k = 1, ng
+    Do j = 1, ng
+      Do i = 1, ng
+        xx = xc(i) - 0.35_sp
+        yy = xc(j) - 0.35_sp
+        zz = xc(k) - 0.35_sp
+        ls(i,j,k) = ShapeRiderKothe(xx,yy,zz)
+      End Do
+    End Do
+  End Do
+
+  Do k = 1, ng 
+    Do j = 1, ng
+      Do i = 1, ng
+        xx = xc(i) - 0.28_sp
+        yy = xc(j) - 0.28_sp
+        zz = xc(k) - 0.28_sp
+        flag = Inout(xx, yy, zz, dx, dx, dx)
+        If (flag .eq. 1) Then
+          phi(i,j,k) = 1.0_sp
+          cx(i,j,k)  = 0.5_sp
+          cy(i,j,k)  = 0.5_sp
+          cz(i,j,k)  = 0.5_sp
+        Elseif (flag .eq. 2) Then
+          phi(i,j,k) = 0.0_sp
+          cx(i,j,k)  = 0.0_sp
+          cy(i,j,k)  = 0.0_sp
+          cz(i,j,k)  = 0.0_sp
+        Elseif (flag .eq. 3) Then
+          gr%level = 1
+          gr%maxlevel = Octreelevel
+          gr%xc(1) = xx
+          gr%xc(2) = yy
+          gr%xc(3) = zz
+          gr%dx(1) = dx
+          gr%dx(2) = dx
+          gr%dx(3) = dx
+          Call VolumeOctree(gr)
+          phi(i,j,k) = gr%vof
+          ! Call VolumeCentroidOctree(gr)
+          ! if ( phi(i,j,k) .gt. epsc ) cx(i,j,k)  = gr%centroid(1) + 0.5_sp
+          ! if ( phi(i,j,k) .gt. epsc ) cy(i,j,k)  = gr%centroid(2) + 0.5_sp
+          ! if ( phi(i,j,k) .gt. epsc ) cz(i,j,k)  = gr%centroid(3) + 0.5_sp
+          n3(1) = ls(i-1,  j,  k) - ls(i+1,  j,  k)
+          n3(2) = ls(  i,j-1,  k) - ls(  i,j+1,  k)
+          n3(3) = ls(  i,  j,k-1) - ls(  i,  j,k+1)
+          Call Normalization1(n3)
+          Call FloodSZ_BackwardC(n3,phi(i,j,k),c3)
+          cx(i,j,k) = c3(1)
+          cy(i,j,k) = c3(2)
+          cz(i,j,k) = c3(3)
+        EndIf
+      End Do
+    End Do
+  End Do
+
+
+  Block
+    Integer :: nn
+    Character(80) :: data_name
+    data_name = 'init'
+    print *, n_vars
+    do nn = 1, n_vars
+      Select Case(Trim(h5_output_field(nn)%groupname))
+      Case('phi')
+        Call HDF5WriteData(h5_output_field(nn), phi,data_name)
+      Case('u')
+        Call HDF5WriteData(h5_output_field(nn), u,data_name)
+      Case('v')
+        Call HDF5WriteData(h5_output_field(nn), v,data_name)
+      Case('w')
+        Call HDF5WriteData(h5_output_field(nn), w,data_name)
+      Case('cx')
+        Call HDF5WriteData(h5_output_field(nn), cx,data_name)
+      Case('cy')
+        Call HDF5WriteData(h5_output_field(nn), cy,data_name)
+      Case('cz')
+        Call HDF5WriteData(h5_output_field(nn), cz,data_name)
+      End Select
+    end do
+  End Block
+
+  Call Visual3DContour(f1=phi)
+  Call Visual3DContour(f1=cx)
+  Call Visual3DContour(f1=cy)
+  Call Visual3DContour(f1=cz)
+
+  Call MPI_FINALIZE(ierr)
+
+End Subroutine InitVolume
