e6561ec6728ef6f6d1baa53831bc70dbdd3d1bad
Zhouteng Ye <yztchina@zju.edu.cn>
2020-07-06 11:51:56 +0800

    a working paraview version

--- tests/ns/gen_paraview.py
+++ tests/ns/gen_paraview.py
@@ -2,6 +2,8 @@ from pyevtk.vtk import VtkFile, VtkRectilinearGrid
 import numpy as np
 from pyevtk.vtk import VtkGroup
 import h5py
+import os
+import sys
 
 f = h5py.File('output.h5','r')
 vars = list(f.keys())
@@ -16,20 +18,26 @@ times = list(f['phi'].keys())
 nx, ny, nz = f['u'][times[0]].shape
 
 #Read data
-x = np.arange(nx)
-y = np.arange(ny)
-z = np.arange(nz)
+x = np.arange(nx) / 20.0
+y = np.arange(ny) / 20.0
+z = np.arange(nz) / 20.0
 
 #Define some parameters
 start, end = (1,1,1), (nx, ny, nz) #Modify 0->1
 
+if (len(sys.argv) ==2):
+    new_dir = sys.argv[1]
+else:
+    new_dir = 'paraview'
+new_data = new_dir + '/data'
+os.system('mkdir -p ' + new_dir)
+os.system('mkdir -p ' + new_data)
 
-#Add to write file. 
+#Add to write file.
 for step in times:
-    filename=str(step)+'.vtr'
-    w = VtkFile(str(step), VtkRectilinearGrid) #evtk_test0
+    filename=new_data + '/'+ str(step)
+    w = VtkFile(filename, VtkRectilinearGrid) #evtk_test0
 
-    # w = VtkFile("./filename", VtkRectilinearGrid) #evtk_test0
     w.openGrid(start = start, end = end)
     w.openPiece( start = start, end = end)
 
@@ -56,12 +64,15 @@ for step in times:
     for key in scalar_vars:
         w.appendData(data = np.array(f[key][step]))
     w.appendData(data = (vx,vy,vz))
-    w.appendData(x).appendData(y).appendData(z)
+    w.appendData(x)
+    w.appendData(y)
+    w.appendData(z)
     w.save()
+    print("file: "+filename+" added")
 
-g = VtkGroup("./group")
+g = VtkGroup(new_dir+"/group")
 for step in times:
-    g.addFile(filepath = str(step)+'.vtr', sim_time = float(step))
+    g.addFile(filepath = new_data + '/' + str(step)+'.vtr', sim_time = float(step))
 g.save()
 
-
+print("group file: group.pvd added")
