26be35070fdfcee6cca4f7990ba9662770446582
ZhoutengYe <yztchiina@zju.edu.cn>
2020-09-01 15:50:40 +0800

    back after long break

---
+++ cases/random_gradient/runeall.sh
@@ -0,0 +1,25 @@
+cd extreme
+python gen_f.py
+./TEST1.exec
+cd ..
+cd uniform
+python gen_f.py
+./TEST1.exec
+cd ..
+cd exponential
+python gen_f.py
+./TEST1.exec
+cd ..
+
+cd extreme
+./TEST2.exec
+./TEST3.exec
+cd ..
+cd uniform
+./TEST2.exec
+./TEST3.exec
+cd ..
+cd exponential
+./TEST2.exec
+./TEST3.exec
+cd ..
