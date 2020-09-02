27ffcd4626998be775894cee90257976bd1def3e
ZhoutengYe <yztchiina@zju.edu.cn>
2020-09-02 20:56:55 +0800

    initial-guess for zalesak

---
+++ cases/Zalesak/exec/50/batch.sh
@@ -0,0 +1,36 @@
+cd MOF
+make 
+cp ../../../inputs/input50.namelist . 
+cp ../../../inputs/input50.h5 . 
+./Zalesak* input50
+cd ..
+cd MOFBFGS
+make 
+cp ../../../inputs/input50.namelist . 
+cp ../../../inputs/input50.h5 . 
+./Zalesak* input50
+cd ..
+cd MOFZY
+make 
+cp ../../../inputs/input50.namelist . 
+cp ../../../inputs/input50.h5 . 
+./Zalesak* input50
+cd ..
+cd MOFLemoineGN
+make 
+cp ../../../inputs/input50.namelist . 
+cp ../../../inputs/input50.h5 . 
+./Zalesak* input50
+cd ..
+cd PLIC
+make 
+cp ../../../inputs/input50.namelist . 
+cp ../../../inputs/input50.h5 . 
+./Zalesak* input50
+cd ..
+cd THINC
+make 
+cp ../../../inputs/input50.namelist . 
+cp ../../../inputs/input50.h5 . 
+./Zalesak* input50
+cd ..
